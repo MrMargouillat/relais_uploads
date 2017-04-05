b58e38862aa1c9ada556390cf0f0efae1fe7e5c4
MrMargouillat <klaja.Raphael@gmail.com>
2017-04-05 12:48:50 +0200

    completing api with language order

--- models/meetings.js
+++ models/meetings.js
@@ -1,10 +1,13 @@
 let connection = require("../config/db")
+let renameProperty = require("../helpers/renameProperty")
+
+
 
 
 class Meeting {
     getAll() {
         return new Promise((resolve, reject) => {
-            connection.query("SELECT *  FROM metings", (err, result) => {
+            connection.query("SELECT *  FROM meetings", (err, result) => {
                 if (err) {
                     throw err
                     reject(err)
@@ -13,6 +16,51 @@ class Meeting {
             })
         })
     }
+    getByLanguage(language) {
+        return new Promise((resolve, reject) => {
+            let q
+            switch (language) {
+                case "fr":
+                    q = "SELECT id, year, place, theme_fr FROM meetings"
+                    break;
+
+                case "en":
+                    q = "SELECT id, year, place, theme_en FROM meetings"
+                    break;
+
+                case "pl":
+                    q = "SELECT id, year, place, theme_pl FROM meetings"
+                    break;
+
+                case "de":
+                    q = "SELECT id, year, place, theme_de FROM meetings"
+                    break;
+
+                default:
+                    reject()
+                    break;
+            }
+
+            connection.query(q, cb)
+
+
+            function cb(err, result) {
+                if (err) {
+                    throw err
+                    reject(err)
+                }
+                let arr = []
+                for (var i = 0; i < result.length; i++) {
+
+                    var obj = result[i];
+                    obj = renameProperty(obj, "theme_" + language, "theme")
+                }
+                resolve(result)
+            }
+        })
+    }
+
+
 }
 
 module.exports = Meeting
